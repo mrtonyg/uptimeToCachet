71b0b9d12bcb21ce182e9fe45c7a0cd87d79d76a
Anthony C George <tony@zignut.com>
2018-01-27 01:02:53 -0500

    Add cachet and uptime client libs

--- fetch.php
+++ fetch.php
@@ -6,14 +6,27 @@
  * Time: 11:06 PM
  */
 require_once 'vendor/autoload.php';
-use Vdhicts\UptimeRobot\Client;
-// Load the configuration once, the URL is optional
-$configuration = new Client\Configuration('u153423-9286d08940508b01e445abd9');
-//, 'OPTIONAL_API_URL');
-// Start the client once
-$client = new Client\Client($configuration);
-// Perform the request, returns a string for further processing
-$response = $client->perform('getMonitors',['limit'=>'50']);
+use Vdhicts\UptimeRobot\Client as uptimeClient;
+use \DivineOmega\CachetPHP\Factories\CachetInstanceFactory;
+
+$cachetendpoint = 'https://status.mediamanaged.com/api/v1/';
+$uptimeendpoint = 'https://api.uptimerobot.com/v2/';
+$cachettoken    = '1STdSGGSrkZFGuSduS2w';
+$uptimetoken='u153423-9286d08940508b01e445abd9';
+
+
+$cachetClient = CachetInstanceFactory::create($cachetendpoint, $cachettoken);
+
+$uptimeConfig = new uptimeClient\Configuration($uptimetoken,$uptimeendpoint);
+$uptimeClient = new uptimeClient\Client($uptimeConfig);
+
+//Load Monitors from uptime
+$response = $uptimeClient->perform('getMonitors',['limit'=>'50']);
+//Load components from cachet
+$cachetComponents = $cachetClient->getAllComponents();      // Components
+$cachetIncidents = $cachetClient->getAllIncidents();        // Incidents
+$cachetMetrics = $cachetClient->getAllMetrics();            // Metrics
+$cachetMetricPoints = $metrics[0]->getAllMetricPoints();      // Metric Points
 //, ['monitors' => '15830-32696-83920']);
 //print $response;
-var_dump(json_decode($response, true));
+//var_dump(json_decode($response, true));
